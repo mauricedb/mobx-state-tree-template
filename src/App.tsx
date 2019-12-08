5b56be09f5d629129cf1f1ac4c4a882ca16c6245
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-12-08 20:40:51 +0100

    Example mobx-state-tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { NumbersStore, NumbersStoreProvider } from "./models";
+
+import AddNumber from "./components/AddNumber";
+import NumbersInfo from "./components/NumbersInfo";
+import NumbersList from "./components/NumbersList";
+
+const numbersStore = NumbersStore.create({});
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
+    <NumbersStoreProvider value={numbersStore}>
+      <div>
+        <AddNumber />
+        <NumbersInfo />
+        <NumbersList />
+        {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
         >
-          Learn React
+        Learn React
         </a>
-      </header>
-    </div>
+      </header> */}
+      </div>
+    </NumbersStoreProvider>
   );
-}
+};
 
 export default App;
